79af3c222b17741ef3c1d0a62d8475cc4c619d8d
Tristan Destoop <tristan.Destoop@gmail.com>
2024-09-24 11:29:10 +0200

    Test de publication de la librairie sans le CSS

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Point d'entrée de ta bibliothèque
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Dossier où les fichiers compilés seront stockés
+    filename: 'bundle.js', // Nom du fichier généré
+    library: 'MaLibrairie', // Nom de la bibliothèque
+    libraryTarget: 'umd', // UMD permet d'être compatible avec différents environnements (CommonJS, AMD, ou directement dans le navigateur)
+    globalObject: 'this' // Utilisé pour UMD dans les environnements où `window` n'est pas disponible
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Extensions JS ou JSX
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Résolution des extensions
+  },
+  externals: {
+    react: 'react', // Exclut React de la compilation, on suppose que React est fourni par l'application qui utilise la bibliothèque
+    'react-dom': 'react-dom'
+  },
+  mode: 'production', // Mode production pour optimiser le bundle
+};
