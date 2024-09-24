7e14decb606e2c5ac1cd976c967155927a867076
Tristan Destoop <tristan.Destoop@gmail.com>
2024-09-24 12:10:04 +0200

    Test de publication sans CSS

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   entry: './src/index.js', // Point d'entrée de ta bibliothèque
   output: {
     path: path.resolve(__dirname, 'dist'), // Dossier où les fichiers compilés seront stockés
-    filename: 'bundle.js', // Nom du fichier généré
-    library: 'MaLibrairie', // Nom de la bibliothèque
+    filename: '[name].js', // Nom du fichier généré
+    library: 'ModaleLibrary', // Nom de la bibliothèque
     libraryTarget: 'umd', // UMD permet d'être compatible avec différents environnements (CommonJS, AMD, ou directement dans le navigateur)
     globalObject: 'this' // Utilisé pour UMD dans les environnements où `window` n'est pas disponible
   },
